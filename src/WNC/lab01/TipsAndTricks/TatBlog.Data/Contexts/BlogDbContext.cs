149d0ae49fdb7fbaec3fe3da236772a1806a424c
Minhlong <2012365@dlu.edu.vn>
2023-03-01 00:10:50 +0700

    Chay

---
+++ src/WNC/lab01/TipsAndTricks/TatBlog.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Mappings;
+
+namespace TatBlog.Data.Contexts
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
